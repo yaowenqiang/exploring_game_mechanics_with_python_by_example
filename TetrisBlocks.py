5bd8c2a3ac4bef65c6482fe869f18773ecc8d16b
yaowenqiang <yaowenqiang111@163.com>
2024-05-13 23:51:13 +0800

    add draw blocks

--- TetrisBlocks.py
+++ TetrisBlocks.py
@@ -8,8 +8,27 @@ blocks = [
     [[0, 1, 3, 6], [0, 1, 2, 5], [2, 5, 7, 8], [3, 6, 7, 8]],  # L 1
     [[1, 2, 5, 8], [5, 6, 7, 8], [8, 3, 6, 7], [0, 1, 2, 3]],  # L 2
     [[4, 6, 7, 8], [0, 3, 4, 6], [0, 1, 2, 4], [2, 4, 5, 8]],  # one on three
-
 ]
+
+class Block:
+    def __init__(self, x ,y):
+        self.x = x
+        self.y = y
+        self.type = 0
+        self.rotation = 0
+
+    def shape(self):
+        return blocks[self.type][self.rotation]
+
+def draw_block():
+    for y in range(3):
+        for x in range(3):
+            if y * 3 + x in block.shape():
+                pygame.draw.rect(screen, (255, 255, 255), [
+                    (x + block.x) * grid_size + x_gap + 1,
+                    (y + block.y) * grid_size + y_gap + 1, grid_size-2, grid_size-2])
+
+
 
 grid_size = 30
 pygame.init()
@@ -25,7 +44,7 @@ y_gap = (screen.get_height() - rows * grid_size) // 2
 pygame.display.set_caption("Tetris")
 
 game_over = False
-
+block = Block(5,6)
 
 def draw_grid(rows, cols, grid_size, x_gap, y_gap):
     for y in range(rows):
@@ -42,6 +61,7 @@ while not game_over:
     screen.fill((0, 0, 0))
 
     draw_grid(rows, cols, grid_size, x_gap, y_gap)
+    draw_block()
     pygame.display.update()
 
 pygame.quit()
